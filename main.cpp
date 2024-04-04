165a95bf434b6d0274a3d9092d1a31eb25e5e895
POLIBITS <112898742+Polibits@users.noreply.github.com>
2024-04-03 22:21:07 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// este é um código padrão do C++
+// ele imprime a mensagem "Bootcamp Polibits 2024!" no terminal
+
+int main() {
+    std::cout << "Bootcamp Polibits 2024!\n";
+    return 0;
+}
