84e157a05f7b6df3845cb7b748a97226c216788f
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2024-04-03 22:48:31 -0300

    cout

---
+++ aulas/aula_01/cout.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// Código exemplo sobre uso de cout
+
+int main() {
+    std::cout << "Bootcamp Polibits 2024!\n";
+    std::cout << "Esperamos que você goste :)\n";
+    std::cout << "Hoje, vamos aprender sobre cout\n\n";
+
+    std::cout << "cin (in = input = entrada) serve para RECEBER dados\n";
+    std::cout << "cout (out = output = saída) serve para ENVIAR dados\n\n";
+
+    // sintaxe básica é
+    // std::cout << "sua mensagem";
+    std::cout << "O cout, quando chamado num terminal, vai imprimir uma mensagem nesse terminal\n";
+
+    // é possível também imprimir várias mensagens, usando a sintaxe:
+    // stt::cout << "mensagem 1" << "mensagem 2" << "mensagem 3" << ... << "mensagem n";
+    std::cout << "Por exemplo" << "\n" << "std::cout << \"p o l i b i t s\";" << "\n";
+    std::cout << "escreve \"p o l i b i t s\" no terminal" << "\n\n";
+
+    std::cout << "Sabe o que significa esse caracter \\n? representa uma quebra de linha\n";
+
+    std::cout << "No geral, existem caracteres especiais, que podemos imprimir usando barra (\\) no texto\n";
+    std::cout << "Por exemplo, o caracter da aspa simples (\') pode ser impresso usando \\' no texto\n\n";
+
+    std::cout << "Existem várias formas de se usar o cout";
+    
+    return 0;
+}
